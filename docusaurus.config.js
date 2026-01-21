f7cfb7915f5f7d08bd201bf55d3e9c455aae8f6f
Danil Pismenny <dapi@users.noreply.github.com>
2026-01-21 10:27:25 +0300

    fix: change baseUrl for docs.kiiiosk.store domain
    - Update baseUrl from /wiki/ to /
    - Update url to docs.kiiiosk.store
    - Fix domain configuration for proper deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://merchantly.github.io',
+  url: 'https://docs.kiiiosk.store',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/wiki/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'merchantly', // Usually your GitHub org/user name.
